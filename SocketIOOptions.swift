31bf20a603ffd02d7e82dec41e1bd05188693bd3
Ricardo Pereira <m@ricardopereira.eu>
2015-04-02 19:29:28 +0100

    init

---
+++ SocketIOOptions.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOOptions.swift
+//  Smartime
+//
+//  Created by Ricardo Pereira on 02/04/2015.
+//  Copyright (c) 2015 Ricardo Pereira. All rights reserved.
+//
+
+import Foundation
+
+class SocketIOOptions {
+    
+}
