3de2721b5cdf2cba23967ba28ceac61a8fc1526b
Ricardo Pereira <m@ricardopereira.eu>
2015-05-26 14:52:45 +0100

    Removed Carthage>Checkouts

--- Carthage/Checkouts/runes/Source/Operators.swift
+++
@@ -1,4 +0,0 @@
-infix operator <^> { associativity left precedence 130 }
-infix operator <*> { associativity left precedence 130 }
-infix operator >>- { associativity left precedence 135 }
-infix operator -<< { associativity right precedence 100 }
